563cf8c48554a0c6f78ca02b8e3a8efac4f6c1f5
unorji kingsley <unorjikingsley@gmail.com>
2023-07-31 16:31:40 +0100

    Made changes to user file

--- user_interactions.rb
+++ user_interactions.rb
@@ -50,15 +50,15 @@ class UserInteractions
 
   def list_numbers()
     list = "
-Please choose an option by entering a number (1-7):
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit
-"
+      Please choose an option by entering a number (1-7):
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit
+      "
     puts list
     gets.chomp
   end
